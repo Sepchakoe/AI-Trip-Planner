8d4f98d9940f660bcd3aa6fa54326906a1dc3539
September Koe <septemberkoe@mac.mynetworksettings.com>
2025-05-01 00:39:04 -0700

    Initial commit

---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="flex items-center mx-45 gap-9">
+      <h1 className="font-extrabold text-[60px] text-[#074813] text-center">
+        Your Dream Trip, Planned by AI Magic{" "}
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
